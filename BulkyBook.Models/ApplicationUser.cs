80ef98bb869ac26594f3f8048d1a773b3c4aa25e
eric wei <ewei337@hotmail.com>
2023-07-24 22:39:53 +1000

    Add user list page. Add user management feature. Add role management feature.

--- BulkyBook.Models/ApplicationUser.cs
+++ BulkyBook.Models/ApplicationUser.cs
@@ -18,8 +18,11 @@ namespace BulkyBook.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+        public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public int? CompanyId { get; set; }
+        public Company? Company { get; set; }
+        [NotMapped]
+        public string Role { get; set; }
     }
 }
---
+++ BulkyBookWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,132 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoleManagement(string userId)
+        {
+            string roleId = _db.UserRoles.FirstOrDefault(ur => ur.UserId == userId).RoleId;
+
+            RoleManagementVM roleVM = new RoleManagementVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _db.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name, 
+                    Value = i.Name,
+                }),
+                CompanyList = _db.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString(),
+                }),
+            };
+
+            roleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(r => r.Id == roleId).Name;
+
+            return View(roleVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+        {
+            string roleId = _db.UserRoles.FirstOrDefault(ur => ur.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(r => r.Id == roleId).Name;
+
+            if (roleManagementVM.ApplicationUser.Role != oldRole)
+            {
+                ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id);
+
+                if (roleManagementVM.ApplicationUser.Role == SD.Role_User_Comp)
+                {
+                    applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId; 
+                }
+                if (oldRole == SD.Role_User_Comp)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                _db.SaveChanges();
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach (var user in userList)
+            {
+                var roleId = userRoles.FirstOrDefault(ur => ur.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(r => r.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new Company { Name = "" };
+                }
+            }
+
+            return Json(new { data = userList });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
+        {
+            var userFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+
+            if (userFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking. User ID not found." });
+            }
+
+            if (userFromDb.LockoutEnd != null && userFromDb.LockoutEnd > DateTime.Now)
+            {
+                userFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                userFromDb.LockoutEnd = DateTime.Now.AddDays(30);
+            }
+            _db.SaveChanges();
+
+            return Json(new { success = true, message = "Lock/Unlock user successful" });
+        }
+        #endregion
+    }
+}
